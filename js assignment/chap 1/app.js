2a024986e1fa23f1025551a397a6d35b9b047f86
wareesha-nasir <57236937+wareesha-nasir@users.noreply.github.com>
2020-06-09 19:55:42 +0000

    Add files via upload

---
+++ js assignment/chap 1/app.js
@@ -0,0 +1,21 @@
+//Task 1
+alert("Welcome to my site");
+
+//Task 2
+alert("Error!... Please enter a valid password");
+
+//Task 3
+alert("Welcome to JS Land \n Happy coding");
+
+//Task 4
+alert("Welcome to JS land");
+confirm("Happy Coding \n Prevent this page from additional dialogs");
+
+//Task 5
+//alert from console here it its code
+alert("hello..I can run js through browser console");
+
+//Task6 and task 7 done in another project  
+
+
+ 
