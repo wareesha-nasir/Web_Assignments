c324a64ce42195484b0ff2f2ad429ec2cb1b3369
wareesha-nasir <57236937+wareesha-nasir@users.noreply.github.com>
2020-06-11 19:18:28 +0000

    Add files via upload

---
+++ js assignment/chap 2/app.js
@@ -0,0 +1,40 @@
+//task 1
+var username;
+
+//task 2
+var myName="Wareesha Nasir"
+
+//task3
+var message="Hello World";
+alert(message);
+//task 4
+let a="Sam";
+let b="20 yrs";
+let c="medical student";
+alert(a);alert(b);alert(c);
+
+//task 5
+var m="pizza";
+var text="";
+for(var i=m.length;i>0;i--){
+  text+= m.slice(0,i)+"\n";
+}
+alert(text);
+//task 6
+var email="warisha.nasir356@gmail.com";
+alert("my email is "+ email);
+
+//task 7 
+var book="A smarter way to learn JavaScript";
+alert("I am trying to learn from Book " + book);
+
+//task 8 this can be done in two ways
+document.write("yay i can write html content through js");
+document.getElementById("text").innerHTML="Yay i can write html content through JS";
+
+//task 9
+var char="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(char);
+
+
+
