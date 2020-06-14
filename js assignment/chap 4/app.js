48010ce4be6eaf11a4685ea820ee1374d5914e51
wareesha-nasir <57236937+wareesha-nasir@users.noreply.github.com>
2020-06-14 11:54:33 +0000

    Add files via upload

---
+++ js assignment/chap 4/app.js
@@ -0,0 +1,21 @@
+//Chapter4
+
+//task 1.
+let a,b,c;
+
+//task 2.
+let firstName,$lastName,_age,dob,city;
+// let 3com, @host, document, %341ruh, !name;
+
+ //task3.
+/*document.body.innerHTML = '<h1>Rules for naming JS variables</h1>';
+
+document.body.innerHTML += `<p>Variable names can only contain , numbers,$ and _.For example : $my_1stVarable</p>
+<p>Variables must begin with letter, $ or _.For example : $name , _name or name</p>
+<p>Variable names are case sensitive</p>
+<p>Variable names should not be JS keywords</p>`;*/
+document.write( `<h1>Rules for namin JS variables</h1>
+<p>Variable names can only contain , numbers,$ and _.For example : $my_1stVarable</p>
+<p>Variables must begin with letter, $ or _.For example : $name , _name or name</p>
+<p>Variable names are case sensitive</p>
+<p>Variable names should not be JS keywords</p>`)
