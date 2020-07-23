d8384ce615de6fe733b401cb6b62266f71634799
wareesha-nasir <57236937+wareesha-nasir@users.noreply.github.com>
2020-07-23 17:59:42 +0000

    Add files via upload

---
+++ js assignment/58-67/app.js
@@ -0,0 +1,76 @@
+//CHAPTER 58-67
+
+//QUESTION 1
+//TASK 1
+// var n = document.getElementById("main-content")
+
+// TASK 2
+// for(var i=0 ;i<n.length ; i++){
+
+//     document.write("<br/>" ,n[i].innerHTML)
+// }
+
+// TASK 3
+// var classes = document.getElementsByClassName("render")
+// for(var j=0 ;j<classes.length ; j++){
+
+//     document.write("<br/>" ,classes[j].innerHTML)
+// }
+// document.write("<br/>")
+// TASK 4
+
+// var input = document.getElementById("first-name").value="wareesha"
+
+// TASK 5
+
+// var last_name = document.getElementById("last-name").value="nasir"
+// var email = document.getElementById("email").value="xyz@yahoo.com"
+
+
+// QUESTION 2
+// TASK1 
+// var form = document.getElementById("form-content").childNodes
+// for(var i=0 ;i<form.length ; i++){
+//     document.write("," + form[i].nodeType)
+// }
+
+// document.write("<br/>")
+
+// TASK 2
+// var lastName = document.getElementById("last-name")
+// document.write("Nodetype of Last Name" , lastName.nodeType)
+
+// for(var l=0 ; l<lastName.childNodes.length ;l++){
+//     document.write("Nodetype of Child node of Last Name" ,lastName[l].nodeType)
+// }
+
+
+// document.write("<br/>")
+
+// TASK 3
+// var lastName = document.getElementById("last-name")
+
+// document.write(lastName.nodeValue = "Update")
+
+
+// document.write("<br/>")
+
+// TASK 4
+// var n = document.getElementById("main-content")
+
+// document.write("First Child of main-content" , n.firstChild.nodeName,"<br/>")
+// document.write("Last Child of main-content" , n.lastChild.nodeName)
+
+// document.write("<br/>")
+
+// TASK 5
+
+// document.write("next sibling of last Name:" ,lastName.nextSibling.nodeName,"<br/>")
+// document.write("previous sibling of last Name:" ,lastName.previousSibling.nodeName)
+
+// document.write("<br/>")
+
+// TASK 6
+// var email = document.getElementById("email")
+// document.write("parent node of email:" ,email.parentNode.nodeName,"<br/>")
+// document.write("node type of email:", email.nodeType)
